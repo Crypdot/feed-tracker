81cc83e2711d510479ed8b0ddd396182b80c5e85
Alexander San Miguel <alexandersanmig@gmail.com>
2022-12-07 09:01:13 +0200

    Added some styling to the Fridge-component. Added a placeholder button for adding new types of feed. - Alex

--- src/scenes/fridge/index.jsx
+++ src/scenes/fridge/index.jsx
@@ -5,7 +5,12 @@ import Header from "../../components/Header";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import {Button} from "@mui/material";
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
+
+/* This is the fridge page. It should display the contents of the fridge, and allow the user to add new feeds to the fridge.
+   Note: Still missing -> The ability to remove feed, and the ability to edit existing feeds.
+*/
 const Fridge = () =>{
     const [fridgeContents, setFridgeContents] = useState([]);
 
@@ -27,6 +32,7 @@ const Fridge = () =>{
     const colors = tokens(theme.palette.mode);
 
     const columns = [
+        { field: "id", headerName: "ID", width: 70, hide: true}, //This allows the us to save the ID of the feed, but not display it.
         { 
             field: "feedName",
             headerName: "Feed",
@@ -48,9 +54,45 @@ const Fridge = () =>{
     return (
     <Box m="20px">
         <Header title="FRIDGE" subtitle="Fridge Interactive Page."/>
+        <Button 
+            onClick={()=>{
+                console.log("This should open a modal for adding a new feed.");
+            }}
+            variant="contained"
+            size="large"
+            color="secondary"
+            endIcon={<AddCircleOutlineIcon/>}
+            
+
+        >Add feed</Button>
         <Box
         m="40px 0 0 0"
         height="75vh"
+        sx={{
+            "& .MuiDataGrid-root": {
+              border: "none",
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: "none",
+            },
+            "& .name-column--cell": {
+              color: colors.greenAccent[300],
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: colors.blueAccent[700],
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: colors.primary[400],
+            },
+            "& .MuiDataGrid-footerContainer": {
+              borderTop: "none",
+              backgroundColor: colors.blueAccent[700],
+            },
+            "& .MuiCheckbox-root": {
+              color: `${colors.greenAccent[200]} !important`,
+            },
+          }}
       >
         <DataGrid checkboxSelection rows={fridgeContents} columns={columns} />
         </Box>
